099daa5685244ac87a596e0d024be87707b59b9e
rishabhAjay <rishabhajay24@gmail.com>
2022-03-22 20:20:37 +0530

    lights off v1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config(); //initialize dotenv
-import http from "http";
 import clientConfig from "./config/clientConfig.js";
 import help from "./components/help.js";
 import transformImage from "./components/transformImage.js";
@@ -14,11 +13,6 @@ clientConfig.on("ready", () => {
   console.log(`Logged in as ${clientConfig.user.tag}!`);
   clientConfig.user.setActivity("=/help");
 });
-
-http.createServer(function (req, res) {
-res.write("I'm alive"); 
-  res.end(); 
-}).listen(8081);
 
 //make sure this line is the last line
 clientConfig.login(process.env.CLIENT_TOKEN); //login bot using token
