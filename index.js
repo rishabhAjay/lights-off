aa63a9af0236df247271b7d02c57a323f8cc5a10
Rishabh Ajay <rishabhajay24@gmail.com>
2022-09-01 12:07:54 +0530

    prepped the bot for deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import help from "./components/help.js";
 import transformImage from "./components/transformImage.js";
 import createInvite from "./components/createInvite.js";
 import cron from "node-cron";
+import http from "http";
 //exported functions
 help();
 transformImage();
@@ -14,6 +15,22 @@ clientConfig.on("ready", () => {
   cron.schedule("*/5 * * * *", () => {
     clientConfig.user.setActivity("=/help");
   });
+});
+
+const port = 3000 || process.env.PORT;
+
+const requestHandler = (request, response) => {
+  response.end("Hello Node.js Server!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+
+  console.log(`server is listening on ${port}`);
 });
 
 //make sure this line is the last line
