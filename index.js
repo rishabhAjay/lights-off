db453bc12604bd62e16688d2876eb80612837633
Rishabh Ajay <rishabhajay24@gmail.com>
2022-09-01 11:41:34 +0530

    prepped the bot for deployment

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import clientConfig from "./config/clientConfig.js";
 import help from "./components/help.js";
 import transformImage from "./components/transformImage.js";
 import createInvite from "./components/createInvite.js";
-
+import cron from "node-cron";
 //exported functions
 help();
 transformImage();
 createInvite();
 clientConfig.on("ready", () => {
   console.log(`Logged in as ${clientConfig.user.tag}!`);
-  clientConfig.user.setActivity("=/help");
+  cron.schedule("*/9 * * * *", () => {
+    clientConfig.user.setActivity("=/help");
+  });
 });
 
 //make sure this line is the last line
