009933401e592a3dc39088d03a0c88fe553bca27
rishabhAjay <rishabhajay24@gmail.com>
2023-05-27 17:17:12 +0530

    replaced http with express

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ import clientConfig from "./config/clientConfig.js";
 import help from "./components/help.js";
 import transformImage from "./components/transformImage.js";
 import createInvite from "./components/createInvite.js";
+import express from "express";
 import cron from "node-cron";
 import http from "http";
 import url from "url";
+const app = express();
+app.use(express.json());
 //exported functions
 help();
 transformImage();
@@ -20,30 +23,21 @@ clientConfig.on("ready", () => {
 
 const port = process.env.PORT || 3000;
 
-const requestHandler = (request, response) => {
-  const baseURL = request.protocol + "://" + request.headers.host + "/";
-  const reqUrl = new URL(request.url, baseURL);
-  if (request.method == "GET") {
-    if (reqUrl == "/health") {
-      const healthcheck = {
-        uptime: process.uptime(),
-        message: "OK",
-        timestamp: Date.now(),
-      };
-      try {
-        res.send(healthcheck);
-      } catch (error) {
-        healthcheck.message = error;
-        res.status(503).send();
-      }
-    } else if (reqUrl === "/") {
-      res.send("Welcome to Lights Off!");
-    }
+app.get("/health", (request, response) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    console.log(Object.keys(response));
+    response.status(200).send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    response.status(503).send({ error });
   }
-};
-
-const server = http.createServer(requestHandler);
-server.listen(port, (err) => {
+});
+app.listen(port, (err) => {
   if (err) {
     return console.log("something bad happened", err);
   }
